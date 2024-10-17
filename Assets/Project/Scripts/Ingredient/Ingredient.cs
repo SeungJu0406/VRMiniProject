b9e6d1f6f9476db53afaf48127753aeb96744490
SeungJu0406 <nss8164@gmail.com>
2024-10-17 17:53:51 +0900

    feat : 잡은 재료 오브젝트 접시와 충돌 금지 구현

--- Assets/Project/Scripts/Ingredient/Ingredient.cs
+++ Assets/Project/Scripts/Ingredient/Ingredient.cs
@@ -13,16 +13,32 @@ public class Ingredient : MonoBehaviour
     [SerializeField] public Ingredient parent;
     [SerializeField] public Ingredient child;
 
+    int ingredientLayer;
+    int ignoreLayer;
+    int socketLayer;
+
     InteractionLayerMask ingredientLayerMask;
     InteractionLayerMask ignoreLayerMask;
     private void Awake()
     {
-        ingredientLayerMask = InteractionLayerMask.GetMask("Ingredient");
-        ignoreLayerMask = InteractionLayerMask.GetMask("Ignore Interactor");
+        InitLayer();
 
         grabInteractable = GetComponent<XRGrabInteractable>();  
     }
-
+    public void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        if (args.interactorObject.transform.gameObject.layer != socketLayer)
+        {
+            gameObject.layer = ignoreLayer;
+        }
+    }
+    public void OnSelectExited(SelectExitEventArgs args)
+    {
+        if (args.interactorObject.transform.gameObject.layer != socketLayer)
+        {
+            gameObject.layer = ingredientLayer;
+        }
+    }
     public void SubscribePlateEvent(Plate plate)
     {
         plate.OnChangeTop += CheckActiveGrabInteratable;
@@ -32,6 +48,7 @@ public class Ingredient : MonoBehaviour
     {
         plate.OnChangeTop -= CheckActiveGrabInteratable;
     }
+
 
     public void CheckActiveGrabInteratable(Plate plate)
     {
@@ -46,4 +63,15 @@ public class Ingredient : MonoBehaviour
             grabInteractable.interactionLayers = ignoreLayerMask;
         }
     }
+
+    void InitLayer()
+    {
+        ingredientLayer = LayerMask.NameToLayer("Ingredient");
+        ignoreLayer = LayerMask.NameToLayer("Ignore Collision");
+        socketLayer = LayerMask.NameToLayer("Socket");
+
+        ingredientLayerMask = InteractionLayerMask.GetMask("Ingredient");
+        ignoreLayerMask = InteractionLayerMask.GetMask("Ignore Interactor");
+    }
+
 }
